4c8f8d9edc8a54971f1d42fabea82da8ee727612
anyappscompany <56433722+anyappscompany@users.noreply.github.com>
2020-11-23 20:56:22 +0200

    Initial commit

---
+++ app/src/main/java/ua/com/anyapps/kt_pirates_notes/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.com.anyapps.kt_pirates_notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
