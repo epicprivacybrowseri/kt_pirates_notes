83452336718d867d08bf4968372b9c283d54e815
anyappscompany <56433722+anyappscompany@users.noreply.github.com>
2020-11-29 19:50:36 +0200

    before flowable

--- app/src/main/java/ua/com/anyapps/kt_pirates_notes/repository/Repository.kt
+++ app/src/main/java/ua/com/anyapps/kt_pirates_notes/repository/Repository.kt
@@ -1,15 +1,8 @@
 package ua.com.anyapps.kt_pirates_notes.repository
 
-import android.content.Context
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import ua.com.anyapps.kt_pirates_notes.di.App
 import ua.com.anyapps.kt_pirates_notes.model.NoteModel
-import ua.com.anyapps.kt_pirates_notes.room.AppDatabase
 import ua.com.anyapps.kt_pirates_notes.room.model.NoteEntity
-import ua.com.anyapps.kt_pirates_notes.utils.TAG
-import javax.inject.Inject
 
 interface Repository{
     fun getById(id: String): NoteEntity
