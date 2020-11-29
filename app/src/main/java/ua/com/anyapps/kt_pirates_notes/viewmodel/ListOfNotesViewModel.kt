b2d02a2921f943073e909a280db7bd7da37934eb
anyappscompany <56433722+anyappscompany@users.noreply.github.com>
2020-11-29 19:46:29 +0200

    before flowable

--- app/src/main/java/ua/com/anyapps/kt_pirates_notes/viewmodel/ListOfNotesViewModel.kt
+++ app/src/main/java/ua/com/anyapps/kt_pirates_notes/viewmodel/ListOfNotesViewModel.kt
@@ -1,14 +1,10 @@
 package ua.com.anyapps.kt_pirates_notes.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import ua.com.anyapps.kt_pirates_notes.model.NoteModel
 import ua.com.anyapps.kt_pirates_notes.repository.Repository
 import ua.com.anyapps.kt_pirates_notes.repository.RepositoryImpl
-import ua.com.anyapps.kt_pirates_notes.utils.TAG
 
 class ListOfNotesViewModel(): ViewModel(){
     private val repository: Repository = RepositoryImpl()
